6575e2db8d9e06a74c257cd038e078ae5cdbef10
Python Test <100.days.python@gmail.com>
2021-03-22 17:17:38 +0530

    Imported OS package

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, CreateNewUser, LoginUser, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
-
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
